55cad8491c76d77e190131c0526059dcefb4675f
Lucas Silva <nascimentolucas786@gmail.com>
2024-08-02 16:01:12 -0300

    refactor: config database connection

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const userRoute = require("./src/routes/user.route");
 const app = express();
+const connectDatabase = require("./src/database/database");
 
 const PORT = 3000;
 
+connectDatabase();
 app.use(express.json());
 app.use("/user", userRoute);
 
---
+++ src/database/database.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+// const connectDatabase = () => {
+//   console.log("Waiting the connection to database");
+
+//   mongoose
+//     .connect(
+//       "mongodb+srv://root:root@dimensional.hdr8jhe.mongodb.net/?retryWrites=true&w=majority&appName=dimensional",
+//     )
+//     .then(() => console.log("MongoDB Atlas Connected"))
+//     .catch((error) => console.log(error));
+// };
+
+async function connectDatabase() {
+  console.log("Waiting the connection to database");
+
+  try {
+    await mongoose.connect(
+      "mongodb+srv://root:root@dimensional.hdr8jhe.mongodb.net/?retryWrites=true&w=majority&appName=dimensional",
+    );
+  } catch (error) {
+    console.log(error);
+  } finally {
+    console.log("MongoDB Atlas Connected");
+  }
+}
+
+module.exports = connectDatabase;
