de5e4ce4a6d513df1ce76e2a1523356dc6a20623
Lucas Silva <nascimentolucas786@gmail.com>
2024-08-02 10:31:08 -0300

    refactor: add the software architecture

--- index.js
+++ index.js
@@ -1,24 +1,8 @@
 const express = require("express");
+const userRoute = require("./src/routes/user.route");
 const app = express();
 
-/* ROUTES
- * HTTP Method
- *  - GET - Pega uma informação
- *  - POST - Cria uma informação
- *  - PUT - Altera toda a informação
- *  - PATCH - Altera parte da informação
- *  - DELETE - Apaga uma informação
- *
- *
- * Name - Um identificador da rota
- * Function (Callback) - Responsável por executar algum comando
- */
-
-app.get("/sum", (req, res) => {
-  const sum = 1 + 1;
-
-  res.send({ sum: sum });
-});
+app.use("/sum", userRoute);
 
 app.listen(3000, () => {
   console.log("Express running at http://localhost:3000");
