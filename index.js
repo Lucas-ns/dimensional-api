3274cf882fe3d01919fc34b341ace307ed4e0c4a
Lucas Silva <nascimentolucas786@gmail.com>
2024-08-02 09:23:25 -0300

    refactor: add route sum

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World! Teste!");
+/* ROUTES
+ * HTTP Method
+ *  - GET - Pega uma informação
+ *  - POST - Cria uma informação
+ *  - PUT - Altera toda a informação
+ *  - PATCH - Altera parte da informação
+ *  - DELETE - Apaga uma informação
+ *
+ *
+ * Name - Um identificador da rota
+ * Function (Callback) - Responsável por executar algum comando
+ */
+
+app.get("/sum", (req, res) => {
+  const sum = 1 + 1;
+
+  res.send({ sum: sum });
 });
 
 app.listen(3000, () => {
