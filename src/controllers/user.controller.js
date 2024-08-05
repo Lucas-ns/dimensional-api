0cb64bbb05c26661c638bc14fce3af4ee936285b
Lucas Silva <nascimentolucas786@gmail.com>
2024-08-05 12:55:34 -0300

    refactor: create global middleware, reusing code

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,4 @@
 const userService = require("../services/user.service");
-const mongoose = require("mongoose");
 
 const create = async (req, res) => {
   const { name, username, password, email } = req.body;
@@ -16,7 +15,7 @@ const create = async (req, res) => {
 
   res.status(201).send({
     message: "User created successfully!",
-    admin: {
+    user: {
       id: user._id,
       name,
       username,
@@ -36,19 +35,23 @@ const findAll = async (req, res) => {
 };
 
 const findById = async (req, res) => {
-  const id = req.params.id;
-
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(400).send({ message: "Invalid ID" });
-  }
-
-  const user = await userService.findByIdService(id);
-
-  if (!user) {
-    return res.status(400).send({ message: "User not found." });
-  }
+  const user = req.user;
 
   res.send(user);
 };
 
-module.exports = { create, findAll, findById };
+const update = async (req, res) => {
+  const { name, username, password, email } = req.body;
+
+  if (!name && !username && !password && !email) {
+    res.status(400).send({ message: "Submit at least one field for update" });
+  }
+
+  const { id, user } = req;
+
+  await userService.updateService(id, name, username, password, email);
+
+  res.send({ message: "User successfully updated!" });
+};
+
+module.exports = { create, findAll, findById, update };
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -6,8 +6,12 @@ const findAllService = () => User.find();
 
 const findByIdService = (id) => User.findById(id);
 
+const updateService = (id, name, username, password, email) =>
+  User.findOneAndUpdate({ _id: id }, { name, username, password, email });
+
 module.exports = {
   createService,
   findAllService,
   findByIdService,
+  updateService,
 };
